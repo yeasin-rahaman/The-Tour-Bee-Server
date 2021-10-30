ce916ad599071bcaf33b5fc819ba20e9d0eef90d
Dev70 <Shanto9585>
2021-10-30 19:47:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.Port || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eeiu8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// console.log('mogno  :',uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('connect to database')
+        const database = client.db("the-tour-bee");
+        const servicesCollection = database.collection('services');
+
+        // Get api
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        // Get singel Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service)
+
+        })
+
+
+
+
+        // POST API
+
+        app.post('/services', async (req, res) => {
+
+            const service = req.body;
+
+            // console.log('Hit the post Api',service);
+
+
+
+
+            const result = await servicesCollection.insertOne(service);
+            //    console.log(result);
+            res.json(result);
+
+        });
+
+        // Delete Api
+
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+
+
+        })
+
+
+
+    }
+    finally {
+        // await client.close()
+    }
+
+
+}
+
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Ranning genious Server')
+
+});
+
+app.listen(port, () => {
+    console.log('Rannig server is port', port);
+});
