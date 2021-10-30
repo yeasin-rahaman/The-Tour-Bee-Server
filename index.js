f67dc844e23879c9c875186661de4381b6532f76
Dev70 <Shanto9585>
2021-10-30 21:23:12 +0600

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 require('dotenv').config();
 
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
